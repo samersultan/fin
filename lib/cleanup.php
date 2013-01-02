6c9e188f15328b8eccef8a7437bea4516da99820
Scott McCauley <scott@ascottmccauley.com>
2013-01-01 21:17:21 -0500

    removed relative urls
    causes more problems with little/no benefit

--- lib/cleanup.php
+++ lib/cleanup.php
@@ -63,7 +63,7 @@ function fin_cleanup_default_category() {
 		$category->name = 'General';
 		$category->slug = strtolower(str_replace('_', ' ', 'general'));
 	}	
-	wp_update_term( $category->term_id, 'category', array( 'slug' => $category->slug, 'name'=> $category->name ) )
+	wp_update_term( $category->term_id, 'category', array( 'slug' => $category->slug, 'name'=> $category->name ) );
 }
 add_action('after_theme_switch','fin_cleanup_default_category');
 
@@ -92,7 +92,7 @@ add_filter('the_generator', '__return_false');
  * Fix Canonical Links
  *
  */
-function fin_cleanup_canonical() {
+function fin_rel_canonical() {
 	global $wp_the_query;
 	
   if (!is_singular()) {
@@ -106,7 +106,8 @@ function fin_cleanup_canonical() {
   $link = get_permalink($id);
   echo "\t<link rel=\"canonical\" href=\"$link\">\n";
 }
-add_action('init', 'fin_cleanup_canonical');
+remove_action('wp_head', 'rel_canonical');
+add_action('wp_head', 'fin_rel_canonical');
 
 /**
  * Clean up output of stylesheet <link> tags
@@ -188,75 +189,6 @@ function fin_get_search_form() {
   locate_template('/templates/searchform.php', true, true);
 }
 add_filter('get_search_form', 'fin_get_search_form');
-
-/**
- * Root relative URLs
- *
- * WordPress likes to use absolute URLs on everything - let's clean that up.
- * Inspired by http://www.456bereastreet.com/archive/201010/how_to_make_wordpress_urls_root_relative/
- *
- * @author Scott Walkinshaw <scott.walkinshaw@gmail.com>
- */
-function fin_root_relative_url($input) {
-  $output = preg_replace_callback(
-    '!(https?://[^/|"]+)([^"]+)?!',
-    create_function(
-      '$matches',
-      // If full URL is home_url("/") and this isn't a subdir install, return a slash for relative root
-      'if (isset($matches[0]) && $matches[0] === home_url("/") && str_replace("http://", "", home_url("/", "http"))==$_SERVER["HTTP_HOST"]) { return "/";' .
-      // If domain is equal to home_url("/"), then make URL relative
-      '} elseif (isset($matches[0]) && strpos($matches[0], home_url("/")) !== false) { return $matches[2];' .
-      // If domain is not equal to home_url("/"), do not make external link relative
-      '} else { return $matches[0]; };'
-    ),
-    $input
-  );
-  var_dump($input);
-
-  return $output;
-}
-
-/**
- * Terrible workaround to remove the duplicate subfolder in the src of <script> and <link> tags
- * Example: /subfolder/subfolder/css/style.css
- */
-function fin_fix_duplicate_subfolder_urls($input) {
-  $output = fin_root_relative_url($input);
-  preg_match_all('!([^/]+)/([^/]+)!', $output, $matches);
-
-  if (isset($matches[1][0]) && isset($matches[2][0])) {
-    if ($matches[1][0] === $matches[2][0]) {
-      $output = substr($output, strlen($matches[1][0]) + 1);
-    }
-  }
-
-  return $output;
-}
-
-$root_rel_filters = array(
-  'bloginfo_url',
-  'theme_root_uri',
-  'stylesheet_directory_uri',
-  'template_directory_uri',
-  'plugins_url',
-  'the_permalink',
-  'wp_list_pages',
-  'wp_list_categories',
-  'wp_nav_menu',
-  'the_content_more_link',
-  'the_tags',
-  'get_pagenum_link',
-  'get_comment_link',
-  'month_link',
-  'day_link',
-  'year_link',
-  'tag_link',
-  'the_author_posts_link'
-);
-
-add_filters($root_rel_filters, 'fin_root_relative_url');
-add_filter('script_loader_src', 'fin_fix_duplicate_subfolder_urls');
-add_filter('style_loader_src', 'fin_fix_duplicate_subfolder_urls');
 
 /**
  * Remove unnecessary self-closing tags
