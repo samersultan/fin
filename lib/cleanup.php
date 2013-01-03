2ddb57589be1e0687c7d5d2e39ef552dcb8a50d4
Scott McCauley <scott@ascottmccauley.com>
2013-01-02 21:19:05 -0500

    Added sitemap.xml

--- lib/cleanup.php
+++ lib/cleanup.php
@@ -120,6 +120,42 @@ Sitemap: ' . $homeURL . '/sitemap.xml';
 add_filter('robots_txt','fin_change_robotstxt', 10, 2);
 
 /**
+ * Create sitemap.xml
+ *
+ */
+function fin_create_sitemap() {
+  $postsForSitemap = get_posts(array(
+    'numberposts' => -1,
+    'orderby' => 'modified',
+    'post_type'  => array('post','page'),
+    'order'    => 'DESC'
+  ));
+  
+  $sitemap = '<?xml version="1.0" encoding="UTF-8"?>';
+  $sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+  
+  foreach($postsForSitemap as $post) {
+    setup_postdata($post);
+    
+    $postdate = explode(" ", $post->post_modified);
+    
+    $sitemap .= '<url>'.
+      '<loc>'. get_permalink($post->ID) .'</loc>'.
+      '<lastmod>'. $postdate[0] .'</lastmod>'.
+      '<changefreq>monthly</changefreq>'.
+    '</url>';
+  }
+  
+  $sitemap .= '</urlset>';
+  
+  $fp = fopen(ABSPATH . "sitemap.xml", 'w');
+  fwrite($fp, $sitemap);
+  fclose($fp);
+}
+add_action("publish_post", "fin_create_sitemap");
+add_action("publish_page", "fin_create_sitemap");
+
+/**
  * Fix Canonical Links
  *
  */
