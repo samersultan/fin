ed0f960ebafff527bf688d3d9f111ff37fe39c5e
Scott McCauley <scott@ascottmccauley.com>
2013-01-04 14:14:55 -0500

    Moved analytics to scripts.php

--- lib/scripts.php
+++ lib/scripts.php
@@ -38,3 +38,30 @@ function fin_scripts() {
 	}
 }
 add_action('wp_enqueue_scripts','fin_scripts', 100);
+
+/**
+ * Add Google Analytics to footer
+ *
+ */
+function fin_analytics() {
+	if(!current_user_can('edit_posts')) {
+		$analyticsNumber = get_option('analytics');
+		$analyticsNumber = 'Hi';
+		if($analyticsNumber != '' && $analyticsNumber != 'UA-#######-#') {
+			$analytics = "<script type='text/javascript'>
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '$analyticsNumber;']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+";
+		}
+	}
+	echo $analytics;
+}
+add_action('wp_footer', 'fin_analytics');
--- templates/footer.php
+++ templates/footer.php
@@ -9,22 +9,4 @@
 	} ?>
 </footer>
 
-<?php /* Analytics */
-if(!current_user_can('administrator')) {
-	$analyticsNumber = get_option('analytics');
-	if($analyticsNumber != '' && $analyticsNumber != 'UA-#######-#') { ?>
-		<script type="text/javascript">
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', '<?php echo $analyticsNumber; ?>']);
-		  _gaq.push(['_trackPageview']);
-		
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-			</script>
-	<?php }
-} ?>
-
 <?php wp_footer(); ?>
