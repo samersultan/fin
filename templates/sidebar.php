397c0bf608207927aba6af47b70f82c871b8dff5
Scott McCauley <scott@ascottmccauley.com>
2013-01-05 08:32:55 -0500

    removing old classes

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -8,11 +8,9 @@
 	$sidebar = 'Default';
 } ?>
 <aside id="sidebar-<?php echo $sidebar; ?>" class="sidebar row" role="complementary">
-	<div class="inner">
-		<?php //check to see if widgets are present if not use 'Default' as a fallback.
-		if(!dynamic_sidebar($sidebar)) {
-			dynamic_sidebar('Default');
-		}
-		?>
-	</div>
+	<?php //check to see if widgets are present if not use 'Default' as a fallback.
+	if(!dynamic_sidebar($sidebar)) {
+		dynamic_sidebar('Default');
+	}
+	?>
 </aside>
