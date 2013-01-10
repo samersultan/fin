ce8e547b9be117381c3b9374c3c4a0b142971166
Scott McCauley <scott@ascottmccauley.com>
2013-01-09 21:24:07 -0500

    Switch back to hierarchical rotator location

--- lib/post-types.php
+++ lib/post-types.php
@@ -63,7 +63,7 @@ function register_taxonomy_location() {
   );
 
   $args = array(
-    'hierarchical'      => false,
+    'hierarchical'      => true,
     'labels'            => $labels,
     'show_ui'           => true,
     'show_admin_column' => true,
