f040256721276144aba5920b42fa3f5f266591d2
Scott McCauley <scott@ascottmccauley.com>
2013-01-12 10:28:55 -0500

    Fixed gallery shortcode

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -32,24 +32,91 @@ remove_shortcode('gallery', 'gallery_shortcode');
 
 // Replace with custom shortcode
 function shortcode_gallery($attr) {
-	global $post, $wp_locale;
+	$post = get_post();
 
-	$output = "";
+  static $instance = 0;
+  $instance++;
 
-	$args = array( 'post_type' => 'attachment', 'numberposts' => -1, 'post_status' => null, 'post_parent' => $post->ID ); 
-	$attachments = get_posts($args);
-	if ($attachments) {
-		$output = '<ul class="block-grid four-up">';
-		foreach ( $attachments as $attachment ) {
-			$output .= '<li>';
-			$att_title = apply_filters( 'the_title' , $attachment->post_title );
-			$output .= wp_get_attachment_link( $attachment->ID , 'thumbnail', true );
-			$output .= '</li>';
-		}
-		$output .= '</ul>';
-	}
+  if (!empty($attr['ids'])) {
+    if (empty($attr['orderby'])) {
+      $attr['orderby'] = 'post__in';
+    }
+    $attr['include'] = $attr['ids'];
+  }
 
-	return $output;
+  $output = apply_filters('post_gallery', '', $attr);
+
+  if ($output != '') {
+    return $output;
+  }
+
+  if (isset($attr['orderby'])) {
+    $attr['orderby'] = sanitize_sql_orderby($attr['orderby']);
+    if (!$attr['orderby']) {
+      unset($attr['orderby']);
+    }
+  }
+
+  extract(shortcode_atts(array(
+    'order'      => 'ASC',
+    'orderby'    => 'menu_order ID',
+    'id'         => $post->ID,
+    'itemtag'    => '',
+    'icontag'    => '',
+    'captiontag' => '',
+    'columns'    => 4,
+    'size'       => 'thumbnail',
+    'include'    => '',
+    'exclude'    => ''
+  ), $attr));
+
+  $id = intval($id);
+
+  if ($order === 'RAND') {
+    $orderby = 'none';
+  }
+
+  if (!empty($include)) {
+    $_attachments = get_posts(array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+
+    $attachments = array();
+    foreach ($_attachments as $key => $val) {
+      $attachments[$val->ID] = $_attachments[$key];
+    }
+  } elseif (!empty($exclude)) {
+    $attachments = get_children(array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+  } else {
+    $attachments = get_children(array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+  }
+
+  if (empty($attachments)) {
+    return '';
+  }
+
+  if (is_feed()) {
+    $output = "\n";
+    foreach ($attachments as $att_id => $attachment) {
+      $output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+    }
+    return $output;
+  }
+
+  $output = '<ul class="block-grid four-up gallery">';
+
+  $i = 0;
+  foreach ($attachments as $id => $attachment) {
+    $link = isset($attr['link']) && 'file' == $attr['link'] ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false);
+
+    $output .= '<li>' . $link;
+    if (trim($attachment->post_excerpt)) {
+      $output .= '<div class="caption hidden">' . wptexturize($attachment->post_excerpt) . '</div>';
+    }
+    $output .= '</li>';
+  }
+
+  $output .= '</ul>';
+
+  return $output;
 }
 add_shortcode('gallery', 'shortcode_gallery');
 
