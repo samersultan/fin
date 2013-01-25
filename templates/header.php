aab87786748751bd6cea0b88038d4fdb1e2191d8
Scott McCauley <scott@ascottmccauley.com>
2013-01-25 13:01:57 -0500

    Link Description to the About page

--- templates/header.php
+++ templates/header.php
@@ -14,6 +14,8 @@
 	<?php } ?>
 	<hgroup>
 		<h1 class="brand"><a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
-		<h2 class="description"><?php echo get_bloginfo('description'); ?></h2>
+		<?php if(get_bloginfo('description') != '') { ?>
+			<h2 class="description"><a href="<?php echo esc_url(get_permalink(get_page_by_path( 'about'))); ?>"><?php echo get_bloginfo('description'); ?></h2>
+		<?php } ?>
 	</hgroup>
 </header>
