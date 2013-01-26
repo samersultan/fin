c31f2cf4368c315a6d541a241ca2671f333b9679
Scott McCauley <scott@ascottmccauley.com>
2013-01-26 09:54:03 -0500

    typo

--- templates/header.php
+++ templates/header.php
@@ -15,7 +15,7 @@
 	<hgroup>
 		<h1 class="brand"><a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
 		<?php if(get_bloginfo('description') != '') { ?>
-			<h2 class="description"><a href="<?php echo esc_url(get_permalink(get_page_by_path( 'about'))); ?>"><?php echo get_bloginfo('description'); ?></h2>
+			<h2 class="description"><a href="<?php echo esc_url(get_permalink(get_page_by_path( 'about'))); ?>"><?php echo get_bloginfo('description'); ?></a></h2>
 		<?php } ?>
 	</hgroup>
 </header>
