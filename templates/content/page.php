de226869c104142611340357ea91804ee1e43cb3
Scott McCauley <scott@ascottmccauley.com>
2013-03-11 12:31:54 -0400

    semantic fix

--- templates/content/page.php
+++ templates/content/page.php
@@ -1,11 +1,11 @@
 <section <?php post_class('single'); ?>>
 	<?php if(!is_front_page()) { ?>
-	<header class="row">
+	<header>
 		<h3 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Bookmark for <?php the_title_attribute(); ?>" class="bookmark"><?php the_title(); ?></a></h3>
 	</header>
 	<?php } ?>
-	<section class="entry-content row">
+	<section class="entry-content">
 		<?php the_content(); ?>
-	</section class="row">
+	</section>
 	<?php edit_post_link('<i class="icon-pencil"></i> edit','<footer>','</footer>'); ?>
 </section>
