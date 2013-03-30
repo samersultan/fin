a1ce1706a56f72ce98e2a17376830b282323f4c2
Scott McCauley <scott@ascottmccauley.com>
2013-03-30 11:36:19 -0400

    Overhaul sidebars and fallback

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,16 +1,26 @@
-<?php if(is_front_page()){
-	$sidebar = 'Home';
+<?php $sidebars = array('Default');
+if(is_front_page()){
+	array_unshift($sidebars,'Home');
 }elseif(is_attachment()){
-	$sidebar = 'Image';
+	array_unshift($sidebars,'Image','Single');
 }elseif(is_single()){
-	$sidebar = 'Single';
-}else{
-	$sidebar = 'Default';
+	array_unshift($sidebars,'Single');
+}elseif(is_single()){
+	array_unshift($sidebars,'Category');
+}elseif(is_tax()){
+	array_unshift($sidebars,'Tag', 'Category');
+}elseif(is_archive()){
+	array_unshift($sidebars,'Archive','Category');
+}elseif(is_author()){
+	array_unshift($sidebars,'Author', 'Single');
+}elseif(is_search()){
+	array_unshift($sidebars,'Search');
 } ?>
-<aside id="sidebar-<?php echo $sidebar; ?>" class="sidebar" role="complementary">
-	<?php //check to see if widgets are present if not use 'Default' as a fallback.
-	if(!dynamic_sidebar($sidebar)) {
-		dynamic_sidebar('Default');
-	}
-	?>
+<aside id="sidebar-<?php echo $sidebars[0]; ?>" class="sidebar" role="complementary">
+	<?php //go through the $sidebars array until one is present
+	foreach ($sidebars as $sidebar) {
+		if(dynamic_sidebar($sidebar)) {
+			break;
+		}
+	} ?>
 </aside>
