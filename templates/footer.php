f91490da3a76864be6f1a2ea5e63bcc0a0b5f4da
Scott McCauley <scott@ascottmccauley.com>
2013-04-11 12:41:21 -0400

    added classes to social media links

--- templates/footer.php
+++ templates/footer.php
@@ -5,32 +5,29 @@
 			<a href="<?php echo esc_url(get_permalink(get_page_by_title('Contact'))); ?>"><i class="icon-phone-sign icon-2x"></i></a>
 			<?php // Facebook
 			if($options['social_facebook'] != '') { ?>
-				<a href="<?php echo 'http://' . preg_replace('`^http://`is', '', $options['social_facebook']); ?>"><i class="icon-facebook-sign icon-2x"></i></a>
-			<?php } ?>
-			<?php // Twitter
+				<a class="facebook" href="<?php echo 'http://' . preg_replace('`^http://`is', '', $options['social_facebook']); ?>"><i class="icon-facebook-sign icon-2x"></i></a>
+			<?php }  
+			// Twitter
 			if($options['social_twitter'] != '') { ?>
-				<a href="<?php echo 'http://' . preg_replace('`^http://`is', '', $options['social_twitter']); ?>"><i class="icon-twitter-sign icon-2x"></i></a>
-			<?php } ?>
-			<?php // Google+
+				<a class="twitter" href="<?php echo 'http://' . preg_replace('`^http://`is', '', $options['social_twitter']); ?>"><i class="icon-twitter-sign icon-2x"></i></a>
+			<?php }  // Google+
 			if($options['social_google_plus'] != '') { ?>
 				<a href="<?php echo 'http://' . preg_replace('`^http://`is', '', $options['social_google_plus']); ?>"><i class="icon-google-plus-sign icon-2x"></i></a>
-			<?php } ?>
-			<?php // Pinterest
+			<?php } 
+			// Pinterest
 			if($options['social_pinterest'] != '') { ?>
-				<a href="<?php echo 'http://' . preg_replace('`^http://`is', '', $options['social_pinterest']); ?>"><i class="icon-pinterest-sign icon-2x"></i></a>
-			<?php } ?>
-			<?php // Linkedin
+				<a class="pinterest" href="<?php echo 'http://' . preg_replace('`^http://`is', '', $options['social_pinterest']); ?>"><i class="icon-pinterest-sign icon-2x"></i></a>
+			<?php }  // Linkedin
 			if($options['social_linkedin'] != '') { ?>
-				<a href="<?php echo 'http://' . preg_replace('`^http://`is', '', $options['social_linkedin']); ?>"><i class="icon-linkedin-sign icon-2x"></i></a>
-			<?php } ?>
-			<?php // Github
+				<a class="linkedin" href="<?php echo 'http://' . preg_replace('`^http://`is', '', $options['social_linkedin']); ?>"><i class="icon-linkedin-sign icon-2x"></i></a>
+			<?php }  // Github
 			if($options['social_github'] != '') { ?>
-				<a href="<?php echo 'http://' . preg_replace('`^http://`is', '', $options['social_github']); ?>"><i class="icon-github-sign icon-2x"></i></a>
+				<a class="github" href="<?php echo 'http://' . preg_replace('`^http://`is', '', $options['social_github']); ?>"><i class="icon-github-sign icon-2x"></i></a>
 			<?php }else {
 				$themeData = wp_get_theme();
 				$developerURL = $themeData->{'Author URI'};
 				if($developerURL != ''){ ?>
-					<a href="<?php echo $developerURL ?>"><i class="icon-desktop icon-2x"></i></a>
+					<a class="developer" href="<?php echo $developerURL ?>"><i class="icon-desktop icon-2x"></i></a>
 				<?php }
 			} ?>
 		</div>
