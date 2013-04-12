8dfab65e7ae372561af042aa2910503a5b4c9c5b
Scott McCauley <scott@ascottmccauley.com>
2013-04-12 13:35:19 -0400

    No more hgroup :(

--- templates/header.php
+++ templates/header.php
@@ -1,16 +1,14 @@
 <?php do_action('get_header'); ?>
 <header id="header" role="banner" <?php if(!is_front_page()) { echo 'class="hide-for-small"'; } ?>>
-	<hgroup>
-		<?php $options = get_option('fin_theme_options');
-		$logo = $options['logo'];
-		if($logo) { ?>
-			<figure class="logo" role="logo"><a href="<?php echo home_url(); ?>/"><img src="<?php echo $logo; ?>"></a></figure>
-		<?php } ?>
-		<h1 class="brand"><a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
-		<?php if(get_bloginfo('description') != '') { ?>
-			<h2 class="description"><a href="<?php echo esc_url(get_permalink(get_page_by_path( 'about'))); ?>"><?php echo get_bloginfo('description'); ?></a></h2>
-		<?php } ?>
-	</hgroup>
+	<?php $options = get_option('fin_theme_options');
+	$logo = $options['logo'];
+	if($logo) { ?>
+		<figure class="logo" role="logo"><a href="<?php echo home_url(); ?>/"><img src="<?php echo $logo; ?>"></a></figure>
+	<?php } ?>
+	<h1 class="brand"><a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
+	<?php if(get_bloginfo('description') != '') { ?>
+		<h2 class="description"><a href="<?php echo esc_url(get_permalink(get_page_by_path( 'about'))); ?>"><?php echo get_bloginfo('description'); ?></a></h2>
+	<?php } ?>
 </header>
 <?php if (has_nav_menu('primary_menu')) { ?>
 	<div class="sticky">
