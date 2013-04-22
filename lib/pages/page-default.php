2afae9de9f878aaeef88d306759d7f19445293da
Scott McCauley <scott@ascottmccauley.com>
2013-04-22 10:23:00 -0400

    update framework

--- lib/pages/page-default.php
+++ lib/pages/page-default.php
@@ -1,23 +1,10 @@
 [row]	
-	[column one-half]
-	
-	[/column]
-	
-	[column one-half]
-	
-	[/column]
+	[column one-half] [/column]
+	[column one-half] [/column]
 [/row]
 
 [row]
-	[column one-third]
-	
-	[/column one-third]
-	
-	[column one-third]
-	
-	[/column one-third]
-	
-	[column one-third]
-	
-	[/column one-third]
+	[column one-third] [/column one-third]
+	[column one-third] [/column one-third]
+	[column one-third] [/column one-third]
 [/row]
